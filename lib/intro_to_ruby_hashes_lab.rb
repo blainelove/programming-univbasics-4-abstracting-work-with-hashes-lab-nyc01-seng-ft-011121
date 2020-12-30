8a797777759e02cfa2ba3f05ce73bee17d0c7f45
Blaine R Love <blainelove94@gmail.com>
2020-12-30 19:35:45 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -1,7 +1,8 @@
 def my_hash_creator(key, value)
+  {key: value}
   # return a hash that includes the key and value parameters passed into this method
 end
-my_hash_creator
+my_hash_creator(key,value)
 def read_from_hash(hash, key)
   # return the correct value using the hash and key parameters
 end
